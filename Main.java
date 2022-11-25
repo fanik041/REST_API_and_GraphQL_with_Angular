30517b57928efb2e1f23927a84caac53f219796a
fahim <fanik041@uottawa.ca>
2022-11-24 20:59:51 -0500

    added solution for java problem

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.example.album.model;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // read user input and assign to the 'num' variable
+        int rows = input.nextInt();
+        createPyramid(rows);
+        }
+    public static void createPyramid(int rows) {
+        int k = 0;
+        for (int i = 1; i <= rows; ++i, k = 0) {
+            for (int space = 1; space <= rows - i; ++space) {
+                System.out.print("  ");
+            }
+
+            while (k != 2 * i - 1) {
+                System.out.print("* ");
+                ++k;
+            }
+
+            System.out.println();
+        }
+    }
+}
